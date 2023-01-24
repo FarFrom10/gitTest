500e644eaddd5e0c7cd183ee879299f5eb00dc0b
Farit <afroid13@gmail.com>
2023-01-24 22:39:49 +0400

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,12 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+// Строгий режим работает по умолчанию, насколько я понял
+"use strict";
+
+
+const someNumber = 2;
+
+console.log(typeof someNumber);
