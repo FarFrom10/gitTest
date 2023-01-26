7d368723ee70c764b6668ec50529b4823f62d0c5
Farit <afroid13@gmail.com>
2023-01-26 19:42:57 +0400

    015: second practice

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -17,12 +17,29 @@ const personalMovieDB = {
 	privat: false,
 };
 
-const answerLastMovie = prompt('Один из последних просмотренных фильмов?', ''),
-	answerMovieRate = +prompt('На сколько оцените его?', ''),
-	answerLastMovieCopy = prompt('Один из последних просмотренных фильмов?', ''),
-	answerMovieRateCopy = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
 
-personalMovieDB.movies[answerLastMovie] = answerMovieRate;
-personalMovieDB.movies[answerLastMovieCopy] = answerMovieRateCopy;
+	const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+	const movieRate = prompt('На сколько оцените его?', '');
+
+	if (lastMovie != null && movieRate != null && lastMovie.length != '' && movieRate.length != '' && lastMovie.length < 50 && movieRate.length < 50) {
+		personalMovieDB.movies[lastMovie] = movieRate;
+		console.log('done');
+	} else {
+		i--;
+		console.log('error');
+	}
+
+};
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
